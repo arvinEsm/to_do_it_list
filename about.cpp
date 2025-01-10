38abce3d02c82355a10aa9beda5568fdb5ba27ff
arvin <esmaeil.arvin.b@gmail.com>
2025-01-10 16:39:38 +0800

    about programm

---
+++ about.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+
+
+
+int main(){
+	std::string choose ; 	
+	std::cout << "plaese select a language  : " << std::endl << "english or deutsch "  ; 
+	std::getline(std::cin , choose ) ;
+       	 	
+       	if (choose == "english" ) {
+		std::cout<<std::endl << std::endl << std::endl <<  "its just a to_do list app for managing tasks , duties , time for who dont want save info in big company servers without any gui (graphical_user_interface ) and any data tracking  and i wanna mention that its a gig app for who taht wanna run everything even youtube or browser in their terminal , and now they have a time managment app in terminal without deamons ||| you order to run and you order to shutdown , thats it |||	" << std::endl << std::endl << "in future i plan to add some feature like qoets and module for personalization . " ; 
+	
+	}else{
+	
+		std::cout<< "not add yet " ;  }
+	
+
+
+
+
+
+
+
+
+
+
+return 0 ; }
