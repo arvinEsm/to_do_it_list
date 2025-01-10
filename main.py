014010dbe38a5d393468a5d34b92fc8d22ef44f4
arvin <esmaeil.arvin.b@gmail.com>
2025-01-10 22:10:33 +0800

    almost done

--- main.py
+++ main.py
@@ -1,18 +1,54 @@
-from lage import *
+from s_and_l import *
+import time
+from datetime import datetime
 
 
 
-global hashtag_value = 0 
+# global hashtag_value = 0 
 
 
 
 
 #main menu 
-button = input (f"what is ur desire ? \n\n1-add a new task : \n2-see ur situations : \n3-tasks u have done  :").strip()
-if button == '1' : 
-    obj = lage() 
-    obj.save_tasks() 
+def menu() : 
+    button = input (f"what is ur desire ? \n\n1-add a new task : \n2-see ur situations : \n3-tasks u have done  :").strip()
+    
+    if button == '1' : 
+    
+        obj1 = save_and_load()
+        show_task = obj1.get_info()
+    
+        print (f"{show_task} seccussfully added .\nur dead_line is in  : ")
+        TT = obj1.get_info("time")
+        print (f"TT\n")
+        print ("warnning : ur delay has a serious consequences  . ") 
+
+    
+
+        Q = input (f"do u wanna back to main menu or quit programm  ? (yes/no) :\n").strip() 
+            if Q == 'yes' : 
+                def menu ()
+            elif Q == "no" : 
+                sys.exit() 
+
+
+    if button == '2' :
+       obj1 = save_and_load()
+       pending = obj1.load(_, )
+       print(f"below taskes is pending :  \n{pending}")
 
 
 
 
+
+
+
+
+
+
+
+
+
+print(f"\n\n\n\n\n\nexact current time , for on_times legend\n  ")
+current_time = datetime.now()
+print("current_time ")
---
+++ s_and_l.py
@@ -0,0 +1,45 @@
+import os 
+import json 
+import datatime from datatime 
+
+
+
+class save_and_load () : 
+    
+
+    def get_info (self) :  
+    self.data ={} 
+    print (f"enter ur task name :\n")
+    task = input ()
+    self.data["task"] = task
+    # find time till dead_line 
+    
+    time = input ("what is ur dead line with ( yyyy,mm,dd  format)  :\n")
+    user_date = datetime.strptime(time , "%Y-%m,%d")
+    differ = user_date - datetime.now()
+    self.data["dead_line"] = differ 
+
+    load(self.data)
+    save(self.data)
+    return self.data 
+
+
+
+
+    def load(self,file) :
+         self.file_name  = file 
+         if os.path.exists(self.file_name):
+             with open(self.file_name , 'r' ,file) as F  :
+                 self.file_name = json.load (F) 
+                 return [] , self.file_name 
+
+    def save (self , packet ) : 
+        self.Packet = packet 
+        with open (self.Packet , 'w') as F :
+            json.dumm(self.Packet , F , indent=4 )
+                
+    
+
+
+
+        
